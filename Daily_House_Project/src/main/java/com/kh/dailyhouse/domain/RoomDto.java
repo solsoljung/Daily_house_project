e63070b9829ea366297e5373b1dd5d25bcca4df5
boohyun <qngus0124@gmail.com>
2020-02-12 15:40:50 +0900

    commit

---
+++ Daily_House_Project/src/main/java/com/kh/dailyhouse/domain/RoomDto.java
@@ -0,0 +1,21 @@
+package com.kh.dailyhouse.domain;
+
+public class RoomDto {
+	
+	private int room_num;
+	private String room_type_num;
+	private String user_email;
+	private String room_location;
+	private int room_score;
+	private int room_price;
+	private String room_explain;
+	private int room_people;
+	private int room_bed;
+	private int room_bathroom;
+	private String room_title;
+	
+	private String user_name;
+	private String user_phone;
+	private String user_pic;
+	
+}
