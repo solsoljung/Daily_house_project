3aaa63e70fc6e96e85707fc0a0c2b6de2751373e
ChaeyeonYu <uoah8230@naver.com>
2020-02-14 16:08:37 +0900

    update RoomMapper.xml, HostMapper.xml fix CyHostDao, impl, CyRoomDao, impl fix CyRoomServiceImpl

--- Daily_House_Project/src/main/java/com/kh/dailyhouse/persistence/CyRoomDaoImpl.java
+++ Daily_House_Project/src/main/java/com/kh/dailyhouse/persistence/CyRoomDaoImpl.java
@@ -15,6 +15,12 @@ public class CyRoomDaoImpl implements CyRoomDao {
 	@Inject
 	public SqlSession sqlSession;
 	
+	// 숙소 등록을 위한 다음 시퀸스 값 알아내기
+	@Override
+	public int getNextRoomNum() throws Exception {
+		return sqlSession.selectOne(NAMESPACE + ".getNextRoomNum");
+	}
+	
 	// 숙소 등록
 	@Override
 	public void registerRoom(RoomVo roomVo) throws Exception {
