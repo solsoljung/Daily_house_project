c55c79f16bf4efef1f70545317a2cf91706e3d01
ChaeyeonYu <uoah8230@naver.com>
2020-02-25 16:38:25 +0900

    fix MyConstants.java

--- Daily_House_Project/java/com/kh/dailyhouse/common/MyConstants.java
+++ Daily_House_Project/java/com/kh/dailyhouse/common/MyConstants.java
@@ -1,8 +1,27 @@
 package com.kh.dailyhouse.common;
 
 public interface MyConstants {
-	public static final String RESERVATION_POINT = "R1";
-	public static final String RESERVATION_CANCLE_POINT = "R2";
+	/*
+	회원 결제
+	회원 환불
+	리뷰
+	회원가입
+	회원 예약 마일리지
+	회원 예약 취소 마일리지
+	호스트 결제 수익
+	호스트 결제 취소
+	*/
+	
+	public static final String USER_PAYMENT = "R1";
+	public static final String USER_PAYMENT_CANCLE = "R2";
+	
 	public static final String WRITE_REVIEW_POINT = "R3";
+	
 	public static final String WELCOME_POINT = "R4";
+	
+	public static final String USER_RESERVATION_POINT = "R5";
+	public static final String USER_RESERVATION_CANCLE_POINT = "R6";
+	
+	public static final String HOST_PAYMENT_REVENUE = "R7";
+	public static final String HOST_PAYMENT_ROLLBACK = "R8";
 }
