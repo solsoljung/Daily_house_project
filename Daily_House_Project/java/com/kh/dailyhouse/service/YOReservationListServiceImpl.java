26fc03db3ea852bda111939591953b987c6b2be3
ChaeyeonYu <uoah8230@naver.com>
2020-02-26 10:23:39 +0900

    commit

--- Daily_House_Project/java/com/kh/dailyhouse/service/YOReservationListServiceImpl.java
+++ Daily_House_Project/java/com/kh/dailyhouse/service/YOReservationListServiceImpl.java
@@ -7,6 +7,7 @@ import javax.inject.Inject;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.kh.dailyhouse.common.MyConstants;
 import com.kh.dailyhouse.domain.PointVo;
 import com.kh.dailyhouse.domain.ReservationVo;
 import com.kh.dailyhouse.domain.TestDto;
@@ -35,14 +36,17 @@ public class YOReservationListServiceImpl implements YOReservationListService {
 	public void insertReservation(TestDto testDto, UserPointDto userPointDto, PointVo pointVo) throws Exception {
 		// 결제시 사용자 포인트 차감, 포인트 테이블에 해당 사용자의 사용내역 기록(R1)
 		dao.user_update_reserv_point(userPointDto);
+		pointVo.setPoint_code(MyConstants.USER_PAYMENT); // R1
 		dao.insertPoint(pointVo);
 		
 		// 결제한 회원에게 결제한 금액의 0.05프로를 포인트로 부여, 포인트 테이블에 적립내역 기록(R5)
 		dao.user_update_reserv_point(userPointDto);
+		pointVo.setPoint_code(MyConstants.USER_RESERVATION_POINT); // R5
 		dao.insertPoint(pointVo);
 		
 		// 호스트에게 포인트 부여, 포인트 테이블에 호스트 포인트 적립내역 기록(R7)
 		dao.host_update_reserv_point(userPointDto);
+		pointVo.setPoint_code(MyConstants.HOST_PAYMENT_REVENUE); // R7
 		dao.insertPoint(pointVo);
 		
 		// 예약 결제
